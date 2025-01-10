58963c4cfc56c4f176b24e1b4c8111f2d5ebae4f
vikash saini <vikashsaini@vikashs-MacBook-Pro.local>
2025-01-10 17:07:55 +0530

    feature:maxium 3 row render only for leaderboard

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
-import React, { useRef, useEffect, useState } from 'react';
-import { View, Animated, StyleSheet, Text, SafeAreaView, Dimensions } from 'react-native';
+import React, {useRef, useEffect, useState} from 'react';
+import {
+  View,
+  Animated,
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
 
-const { width, height } = Dimensions.get('window');
+const {width, height} = Dimensions.get('window');
 const ITEM_HEIGHT = 60;
-const ITEM_MARGIN = 4;
+const ITEM_MARGIN = 8;
 const VISIBLE_ITEMS = 3;
-const VIEWPORT_HEIGHT = ITEM_HEIGHT * VISIBLE_ITEMS + ITEM_MARGIN * (VISIBLE_ITEMS - 1);
+const VIEWPORT_HEIGHT =
+  ITEM_HEIGHT * VISIBLE_ITEMS + ITEM_MARGIN * (VISIBLE_ITEMS - 1);
 
 interface RankItem {
   id: number;
@@ -19,85 +28,225 @@ interface RankItem {
 export default function App() {
   const scrollAnim = useRef(new Animated.Value(0)).current;
   const bounceAnim = useRef(new Animated.Value(0)).current;
-
+  const [status, setStatus] = useState('before'); //before, running, after
+  const scrollY = useRef(new Animated.Value(0)).current;
+  const bounceAnim1 = useRef(new Animated.Value(1)).current;
   const [data] = useState<RankItem[]>(
-    Array(20).fill(0).map((_, i) => ({
-      id: i + 1,
-      rank: i + 1,
-      name: i === 12 ? "Kmonkey11" : `Player ${i + 1}`,
-      score: "434,000",
-      bonus: "$12",
-      isUser: i === 12
-    }))
+    Array(20)
+      .fill(0)
+      .map((_, i) => ({
+        id: i + 1,
+        rank: i + 1,
+        name: i === 12 ? '13th player' : `Player ${i + 1}`,
+        score: '434,000',
+        bonus: '$12',
+        isUser: i === 12,
+      })),
   );
-
+  console.log(
+    '🚀 ~ App ~ data:',
+    data.filter(item => item.rank >= 7 && item.rank <= 14),
+  );
   useEffect(() => {
+    scrollY.addListener(({value}) => {
+      // Calculate the scale factor based on scroll position and time
+      const scaleFactor = 1 + (value / 100) * 0.2; // Adjust the scaling factor as needed
+      Animated.timing(bounceAnim, {
+        toValue: scaleFactor,
+        duration: 3000,
+        useNativeDriver: true,
+      }).start();
+    });
+  }, [scrollY]);
+
+  const onStartAnimate = () => {
     setTimeout(() => {
       // Main scroll animation
       Animated.timing(scrollAnim, {
-        toValue: (13 - 7) * (ITEM_HEIGHT + ITEM_MARGIN),
-        duration: 1000,
+        toValue: 3 * (ITEM_HEIGHT + ITEM_MARGIN),
+        duration: 2000,
         useNativeDriver: true,
       }).start();
 
-      // Continuous bounce animation
+      // bounce animation for rank 13  while scrolling whenever it hovers other ranks
       Animated.loop(
         Animated.sequence([
           Animated.timing(bounceAnim, {
+            toValue: 8,
+            duration: 500,
+            useNativeDriver: true,
+          }),
+          // height a bit bigger and samller to make it bounce effect
+          Animated.timing(bounceAnim, {
+            toValue: 0,
+            duration: 500,
+            useNativeDriver: true,
+          }),
+        ]),
+      ).start();
+      // stop bounce animation after 2 seconds
+      setTimeout(() => {
+        Animated.timing(bounceAnim, {
+          toValue: 0,
+          duration: 150,
+          useNativeDriver: true,
+        }).start();
+      }, 2000);
+
+      const bounceAnim1Steps = Array(10)
+        .fill(0)
+        .map((_, i) => {
+          return new Promise((resolve, reject) => {
+            setTimeout(() => {
+              Animated.timing(bounceAnim1, {
+                toValue: i % 2 === 0 ? 1.1 : 1,
+                duration: 1000 / 10,
+                useNativeDriver: true,
+              }).start(() => {
+                resolve({status: 'done'});
+              });
+            }, i * 200);
+          });
+        });
+      Promise.all(bounceAnim1Steps).then(() => {
+        setTimeout(() => {
+          Animated.timing(bounceAnim1, {
             toValue: 1,
             duration: 150,
             useNativeDriver: true,
-          }),
-          Animated.spring(bounceAnim, {
-            toValue: 0,
-            tension: 100,
-            friction: 8,
-            useNativeDriver: true,
-          })
-        ])
-      ).start();
+          }).start();
+        }, 2000);
+      });
+
+      // stop bounce animation after 2 seconds
+      setTimeout(() => {
+        Animated.timing(bounceAnim1, {
+          toValue: 1,
+          duration: 150,
+          useNativeDriver: true,
+        }).start();
+      }, 2000);
     }, 1000);
-  }, []);
+  };
 
   const renderRankItem = (item: RankItem) => {
     const translateY = scrollAnim.interpolate({
-      inputRange: [0, (13 - 7) * (ITEM_HEIGHT + ITEM_MARGIN)],
-      outputRange: [0, (13 - 7) * (ITEM_HEIGHT + ITEM_MARGIN)],
+      inputRange: [
+        0,
+        ITEM_HEIGHT + ITEM_MARGIN,
+        3 * (ITEM_HEIGHT + ITEM_MARGIN),
+      ],
+      outputRange: [
+        0,
+        ITEM_HEIGHT + ITEM_MARGIN,
+        1 * (ITEM_HEIGHT + ITEM_MARGIN),
+      ],
     });
 
     const bounceTranslateY = bounceAnim.interpolate({
-      inputRange: [0, 1],
+      inputRange: [0, 8],
       outputRange: [0, 8], // Bounce height
     });
 
+    Animated.spring(bounceAnim, {
+      toValue: 1.1,
+      friction: 4,
+      tension: 40,
+      useNativeDriver: true,
+    }).start();
+
+    const animatedStyle = {
+      transform: [{scale: bounceAnim}],
+    };
     return (
       <Animated.View
         key={item.id}
         style={[
           styles.rankItem,
-          item.rank === 13 && styles.rank13Item,
+          animatedStyle,
           {
             transform: [
-              { translateY: item.rank === 13 ? 0 : translateY },
-              { translateY: item.rank === 13 ? 0 : bounceTranslateY }
-            ]
-          }
-        ]}
-      >
+              {translateY: translateY},
+              {translateY: item.rank === 13 ? 0 : bounceTranslateY},
+            ],
+          },
+        ]}>
         <View style={styles.rankContent}>
-          <Text style={[styles.rankText, item.rank === 13 && styles.rank13Text]}>
-            {item.rank}
-          </Text>
+          {item.rank === 13 ? null : (
+            <>
+              <Text>{item.rank}</Text>
+              <View style={styles.playerInfo}>
+                <Text
+                  style={[
+                    styles.nameText,
+                    item.rank === 13 && styles.rank13Text,
+                  ]}>
+                  {item.name}
+                </Text>
+                <Text
+                  style={[
+                    styles.scoreText,
+                    item.rank === 13 && styles.rank13Text,
+                  ]}>
+                  {item.score}
+                </Text>
+              </View>
+              <View
+                style={[
+                  styles.bonusContainer,
+                  item.rank === 13 && styles.rank13BonusContainer,
+                ]}>
+                <Text
+                  style={[
+                    styles.bonusText,
+                    item.rank === 13 && styles.rank13BonusText,
+                  ]}>
+                  BONUS {item.bonus}
+                </Text>
+              </View>
+            </>
+          )}
+        </View>
+      </Animated.View>
+    );
+  };
+
+  const renderMyRank = (item: RankItem) => {
+    return (
+      <Animated.View
+        key={item.id}
+        style={[
+          styles.rankItem,
+          styles.rank13Item,
+          // {
+          //   height: ITEM_HEIGHT * 1.1,
+          // },
+          {
+            transform: [{scale: bounceAnim1}],
+          },
+        ]}>
+        <View style={styles.rankContent}>
+          <Text>{item.rank}</Text>
           <View style={styles.playerInfo}>
-            <Text style={[styles.nameText, item.rank === 13 && styles.rank13Text]}>
+            <Text
+              style={[styles.nameText, item.rank === 13 && styles.rank13Text]}>
               {item.name}
             </Text>
-            <Text style={[styles.scoreText, item.rank === 13 && styles.rank13Text]}>
+            <Text
+              style={[styles.scoreText, item.rank === 13 && styles.rank13Text]}>
               {item.score}
             </Text>
           </View>
-          <View style={[styles.bonusContainer, item.rank === 13 && styles.rank13BonusContainer]}>
-            <Text style={[styles.bonusText, item.rank === 13 && styles.rank13BonusText]}>
+          <View
+            style={[
+              styles.bonusContainer,
+              item.rank === 13 && styles.rank13BonusContainer,
+            ]}>
+            <Text
+              style={[
+                styles.bonusText,
+                item.rank === 13 && styles.rank13BonusText,
+              ]}>
               BONUS {item.bonus}
             </Text>
           </View>
@@ -113,20 +262,31 @@ export default function App() {
           style={[
             styles.scrollContainer,
             {
-              transform: [{ translateY: scrollAnim }]
-            }
-          ]}
-        >
+              transform: [{translateY: scrollAnim}],
+            },
+          ]}>
           {data
             .filter(item => item.rank >= 7 && item.rank <= 14)
-            .sort((a, b) => a.rank - b.rank)
             .map(item => {
-              if (item.rank === 13) return null;
               return renderRankItem(item);
             })}
         </Animated.View>
-        {renderRankItem(data.find(item => item.rank === 13)!)}
+        {renderMyRank(data.find(item => item.rank === 13)!)}
       </View>
+      <TouchableOpacity
+        onPress={() => {
+          onStartAnimate();
+        }}>
+        <Text
+          style={{
+            textAlign: 'center',
+            color: '#FFFFFF',
+            fontSize: 24,
+            marginTop: 100,
+          }}>
+          Start
+        </Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
@@ -140,29 +300,32 @@ const styles = StyleSheet.create({
   viewportContainer: {
     height: VIEWPORT_HEIGHT,
     overflow: 'hidden',
+    backgroundColor: 'grey',
   },
   scrollContainer: {
     position: 'absolute',
     left: 0,
     right: 0,
-    top: -(12 * (ITEM_HEIGHT + ITEM_MARGIN)),
+    top: -(5 * (ITEM_HEIGHT + ITEM_MARGIN)),
   },
   rankItem: {
     height: ITEM_HEIGHT,
-    backgroundColor: '#1A1A2E',
-    marginHorizontal: 16,
+    // backgroundColor: '#1A1A2E',
+    backgroundColor: 'blue',
+    marginLeft: 30,
+    marginHorizontal: 20,
     marginVertical: ITEM_MARGIN / 2,
     borderRadius: 8,
   },
   rank13Item: {
     position: 'absolute',
-    left: 16,
-    right: 16,
+    left: -20,
+    right: 0,
     top: ITEM_HEIGHT + ITEM_MARGIN,
     backgroundColor: '#2A2A3E',
     borderWidth: 1,
     borderColor: '#4CAF50',
-    transform: [{ scale: 1.1 }],
+    transform: [{scale: 1.1}],
     zIndex: 1,
   },
   rankContent: {
